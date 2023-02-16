9cbfb0a08d07f671f133cadbb63328ec7fa327c9
Ouafae1 <wafae.mess@gmail.com>
2023-02-16 14:09:06 -0800

    h

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,12 @@ int main(void)
 	long int b;
 	long long int c;
 	float d;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(x));
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", sizeof(d));
 
-    return (0);
+	return (0);
 }
