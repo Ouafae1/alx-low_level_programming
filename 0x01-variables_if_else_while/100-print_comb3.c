e1374327ac62bb38ff8d7e504f85679b02ed7ae2
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 09:15:53 -0800

    h

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	int x,y=0;
+	int x, y = 0;
 
 	for (x = 0; x <= 8; x++)
 	{
@@ -24,7 +24,7 @@ int main(void)
 						continue;
 					putchar(',');
 					putchar(' ');
-				}	
+				}
 		}
 	}
 	putchar('\n');
