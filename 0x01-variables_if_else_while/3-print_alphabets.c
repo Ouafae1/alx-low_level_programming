09083bd916d537c89a4c09858a8a0e5b7cf156d9
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 07:29:11 -0800

    h

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-        int x;
+	int x;
 
-        for (x = 'A'; x <= 'Z'; x++)
-        {
-        int lower_x = tolower(x);
-
-        putchar(lower_x);
-        }
-        for (x = 'A'; x <= 'Z'; x++)
-        {
-        putchar(x);
-        }
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	int lower_x = tolower(x);
+	
+	putchar(lower_x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(x);
+	}
 	putchar('\n');
 	return (0);
 }
