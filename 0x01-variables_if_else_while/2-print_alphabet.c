905e8cbef36e50867615277ebdb5db64e142e31d
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 07:19:10 -0800

    h

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,10 +12,12 @@ int main(void)
 {
 	int x;
 
-	for(x = 'A'; x <= 'Z'; x++)
+	for( x = 'A'; x <= 'Z'; x++)
 	{
 	int lower_x = tolower(x);
+
 	putchar(lower_x);
 	}
+	putchar('\n');
 	return (0);
 }
