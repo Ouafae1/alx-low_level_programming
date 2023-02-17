0ccc765da37993d6ad2f16f7c3cf02f974002542
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 08:03:02 -0800

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'Z'; x >= 'A'; x--)
+	{
+	int lower_x = tolower(x);
+
+	putchar(lower_x);
+	}
+	putchar('\n');
+	return (0);
+}
