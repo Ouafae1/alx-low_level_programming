de15a6cd173cfbcc4e0b3d264cfed66d41cafdd5
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 08:51:38 -0800

    h

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x,y=0;
+
+	for (x = 0; x <= 8; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+			if (x == 8,y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
