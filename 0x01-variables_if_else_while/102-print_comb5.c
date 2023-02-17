7c18d4a85fe661f7f3a3872fba8153c66dd8ba54
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 15:28:27 -0800

    h

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,17 @@
 int main(void)
 {
 	int x, y;
+
 	for (x = 0; x <= 98; x++)
 	{
-		for (y = x+1; y <= 99; y++)
+		for (y = x + 1; y <= 99; y++)
 		{
-		putchar('0' + (x / 10) );
+		putchar('0' + (x / 10));
 		putchar('0' + (x % 10));
 		putchar(' ');
 		putchar('0' + (y / 10));
 		putchar('0' + (y % 10));
-		if (x == 98 && y == 99 )
+		if (x == 98 && y == 99)
 			continue;
 		putchar(',');
 		putchar(' ');
