22575ee7ab52628226053b3ede921951ebe2d2cd
Ouafae1 <wafae.mess@gmail.com>
2023-02-17 07:40:35 -0800

    h

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	int lower_x = tolower(x);
+
+	if (x != 'Q' && x != 'E')
+	{
+	putchar(lower_x);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
