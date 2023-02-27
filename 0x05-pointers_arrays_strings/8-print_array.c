a69d11e42222856e57d204fd896b93bbab9aa3cf
Ouafae1 <wafae.mess@gmail.com>
2023-02-27 13:14:21 -0800

    j

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_array- prints n elements of an array of integers, followed by a new line.
+ *
+ * @a,n: int.
+ */
+
+void print_array(int *a, int n)
+{
+	int len = 0, i;
+
+	while (a[len] != 0)
+		len++;
+	for (i = 0; i < n ; i++)
+		_putchar (str[i]);
+	_putchar ('\n');
+}
