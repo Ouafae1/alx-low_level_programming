500f919e7bb6841c1f9fa5b67c19ebe7e9054ef5
Ouafae1 <wafae.mess@gmail.com>
2023-02-27 11:20:46 -0800

    j

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *puts2- prints every other character of a string, starting with the first character, followed by a new line.
+ *puts2- prints every other character of a string,
+ *starting with the first character, followed by a new line.
  *
  * @str: string.
  */
@@ -14,4 +15,5 @@ void puts2(char *str)
 		len++;
 	for (i = 0; i < len; i += 2)
 		_putchar (str[i]);
+	_putchar ('\n');
 }
