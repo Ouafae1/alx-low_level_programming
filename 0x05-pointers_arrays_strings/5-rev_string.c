7057c46f56323def55f35244d5bc24de1deb8387
Ouafae1 <wafae.mess@gmail.com>
2023-02-27 09:58:02 -0800

    h

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,3 +9,13 @@
 
 void rev_string(char *s)
 {
+	int len = 0, i;
+	char t;
+
+	while (s[len] != 0)
+		len++;
+	for (i = len - 1; i >= len / 2; i--)
+		t = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = t;
+}
