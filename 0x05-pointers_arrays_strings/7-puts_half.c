d37ba363f5085d04c6921bb7a280514a39af3a04
Ouafae1 <wafae.mess@gmail.com>
2023-02-27 12:26:33 -0800

    h

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,15 @@
 
 void puts_half(char *str)
 {
-	int len = 0, i;
+	int len = 0, i, m;
 
 	while (str[len] != 0)
 		len++;
-	for (i = len - 1; i >= len / 2; i--)
+	if ((len % 2) == 0)
+		j = len / 2;
+	else
+		j = (len - 1) / 2;
+	for (i = j; i < len; i++)
 		_putchar (str[i]);
 _putchar ('\n');
 }
