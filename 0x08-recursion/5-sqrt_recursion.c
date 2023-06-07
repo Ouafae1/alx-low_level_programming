20c8ea4b64f5abbea9687be8a0c72e07f1481722
Ouafae1 <wafae.mess@gmail.com>
2023-06-07 15:22:42 -0700

    h

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,13 +4,21 @@
  * @n: int
  * Return:square root of a number
  */
+/**
+ * rcr - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
 int rcr(int n, int i)
 {
-        if ( i * i == n)
-                return (i);
-        if ( i * i > 0)
-                return (-1);
-        return (rcr (n, i + 1));
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (rcr(n, i + 1));
 }
 int _sqrt_recursion(int n)
 {
