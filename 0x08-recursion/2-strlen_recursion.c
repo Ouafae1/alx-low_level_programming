2763185d01a90b9a0307ebe4c5b7c4496949e983
Ouafae1 <wafae.mess@gmail.com>
2023-06-07 13:15:53 -0700

    h

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen_recursion - function that returns the length of a string.
+ * @s : string
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s)
+	{
+		l = l + 1;
+		_strlen_recursion(s + 1)
+	}
+	return (l);
+}
